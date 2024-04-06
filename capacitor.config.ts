f6fd1218f6cfa4b69193f62d42bb90cf914b6f16
akkoo <developerakshayjadhav@gmail.com>
2024-04-06 16:06:43 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Admin-Restaurant',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
